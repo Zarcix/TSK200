53773fedd2a4503b720b53ac7e38ceac61109493
Zarcix <mrslime43@gmail.com>
2025-04-02 00:38:01 -0700

    Add jump tests

--- test/tsk/node_jump.c
+++ test/tsk/node_jump.c
@@ -1,6 +1,5 @@
 #include <criterion/criterion.h>
 #include <criterion/assert.h>
-#include <stdio.h>
 
 #include "../test_statics.h"
 
@@ -62,6 +61,7 @@ Test(NODE_FUNCTIONALITY, JMP, .init=setup_node, .fini=teardown_node) {
 }
 
 Test(NODE_FUNCTIONALITY, JEZ, .init=setup_node, .fini=teardown_node) {
+    char* labelName = strdup("TOP");
     { // MOV -1 ACC
         Instruction inst = {
             .operation=MOV,
@@ -75,7 +75,6 @@ Test(NODE_FUNCTIONALITY, JEZ, .init=setup_node, .fini=teardown_node) {
         toTest->instructionList[0] = inst;
     }
     { // TOP:
-        char* labelName = strdup("TOP");
         int* labelLoc = malloc(sizeof(int));
         *labelLoc = 1;
         Instruction inst = {
@@ -108,30 +107,440 @@ Test(NODE_FUNCTIONALITY, JEZ, .init=setup_node, .fini=teardown_node) {
     toTest->instructionCount = 4;
 
     // If this is not added properly, the rest will not work
-    cr_assert_eq(*(int*)hashmap_get(&toTest->labelMap, "TOP", strlen("TOP")), 1);
+    cr_assert_eq(*(int*)hashmap_get(&toTest->labelMap, labelName, strlen(labelName)), 1);
 
     for (int _ = 0; _ < 20; _++) {
         node_tick(toTest);
     }
+    free(labelName);
+
+    cr_expect_eq(toTest->ACC, -1);
+    cr_expect_eq(toTest->instructionPointer, 2);
+}
+
+Test(NODE_FUNCTIONALITY, JNZ, .init=setup_node, .fini=teardown_node) {
+    char* labelName = strdup("TOP");
+    { // MOV -5 ACC
+        Instruction inst = {
+            .operation=MOV,
+
+            .src.type=NUMBER,
+            .src.value.dataVal=-5,
+
+            .dest.type=PORT,
+            .dest.value.dataPort=ACC
+        };
+        toTest->instructionList[0] = inst;
+    }
+    { // TOP:
+        int* labelLoc = malloc(sizeof(int));
+        *labelLoc = 1;
+        Instruction inst = {
+            .operation=LABEL,
+            .src.type=STRING,
+            .src.value.dataStr=labelName,
+        };
+        hashmap_put(&toTest->labelMap, labelName, strlen(labelName), labelLoc);
+
+        toTest->instructionList[1] = inst;
+    }
+    { // ADD 1
+        Instruction inst = {
+            .operation=ADD,
+
+            .src.type=NUMBER,
+            .src.value.dataVal=1,
+        };
+        toTest->instructionList[2] = inst;
+    }
+    { // JNZ TOP
+        Instruction inst = {
+            .operation=JNZ,
+
+            .src.type=STRING,
+            .src.value.dataStr=strdup("TOP"),
+        };
+        toTest->instructionList[3] = inst;
+    }
+
+    toTest->instructionCount = 4;
+
+    // If this is not added properly, the rest will not work
+    cr_assert_eq(*(int*)hashmap_get(&toTest->labelMap, labelName, strlen(labelName)), 1);
+
+    for (int _ = 0; _ < 15; _++) {
+        node_tick(toTest);
+    }
+    free(labelName);
+
+    cr_expect_eq(toTest->ACC, -4);
+    cr_expect_eq(toTest->instructionPointer, 3);
+}
+
+Test(NODE_FUNCTIONALITY, JGZ, .init=setup_node, .fini=teardown_node) {
+    char* labelName = strdup("TOP");
+    { // MOV 5 ACC
+        Instruction inst = {
+            .operation=MOV,
+
+            .src.type=NUMBER,
+            .src.value.dataVal=5,
+
+            .dest.type=PORT,
+            .dest.value.dataPort=ACC
+        };
+        toTest->instructionList[0] = inst;
+    }
+    { // TOP:
+        int* labelLoc = malloc(sizeof(int));
+        *labelLoc = 1;
+        Instruction inst = {
+            .operation=LABEL,
+            .src.type=STRING,
+            .src.value.dataStr=labelName,
+        };
+        hashmap_put(&toTest->labelMap, labelName, strlen(labelName), labelLoc);
+
+        toTest->instructionList[1] = inst;
+    }
+    { // SUB 1
+        Instruction inst = {
+            .operation=SUB,
+
+            .src.type=NUMBER,
+            .src.value.dataVal=1,
+        };
+        toTest->instructionList[2] = inst;
+    }
+    { // JGZ TOP
+        Instruction inst = {
+            .operation=JGZ,
+
+            .src.type=STRING,
+            .src.value.dataStr=strdup("TOP"),
+        };
+        toTest->instructionList[3] = inst;
+    }
+
+    toTest->instructionCount = 4;
+
+    // If this is not added properly, the rest will not work
+    cr_assert_eq(*(int*)hashmap_get(&toTest->labelMap, labelName, strlen(labelName)), 1);
+
+    for (int _ = 0; _ < 15; _++) {
+        node_tick(toTest);
+    }
+    free(labelName);
+
+    cr_expect_eq(toTest->ACC, 4);
+    cr_expect_eq(toTest->instructionPointer, 3);
+}
+
+Test(NODE_FUNCTIONALITY, JLZ, .init=setup_node, .fini=teardown_node) {
+    char* labelName = strdup("TOP");
+    { // MOV -5 ACC
+        Instruction inst = {
+            .operation=MOV,
+
+            .src.type=NUMBER,
+            .src.value.dataVal=-5,
+
+            .dest.type=PORT,
+            .dest.value.dataPort=ACC
+        };
+        toTest->instructionList[0] = inst;
+    }
+    { // TOP:
+        int* labelLoc = malloc(sizeof(int));
+        *labelLoc = 1;
+        Instruction inst = {
+            .operation=LABEL,
+            .src.type=STRING,
+            .src.value.dataStr=labelName,
+        };
+        hashmap_put(&toTest->labelMap, labelName, strlen(labelName), labelLoc);
+
+        toTest->instructionList[1] = inst;
+    }
+    { // ADD 1
+        Instruction inst = {
+            .operation=ADD,
+
+            .src.type=NUMBER,
+            .src.value.dataVal=1,
+        };
+        toTest->instructionList[2] = inst;
+    }
+    { // JLZ TOP
+        Instruction inst = {
+            .operation=JLZ,
+
+            .src.type=STRING,
+            .src.value.dataStr=strdup("TOP"),
+        };
+        toTest->instructionList[3] = inst;
+    }
+
+    toTest->instructionCount = 4;
+
+    // If this is not added properly, the rest will not work
+    cr_assert_eq(*(int*)hashmap_get(&toTest->labelMap, labelName, strlen(labelName)), 1);
+
+    for (int _ = 0; _ < 15; _++) {
+        node_tick(toTest);
+    }
+    free(labelName);
+
+    cr_expect_eq(toTest->ACC, -4);
+    cr_expect_eq(toTest->instructionPointer, 3);
+}
+
+Test(NODE_FUNCTIONALITY, JRO, .init=setup_node, .fini=teardown_node) {
+    char* labelName = strdup("TOP");
+    { // ADD 2
+        Instruction inst = {
+            .operation=ADD,
+
+            .src.type=NUMBER,
+            .src.value.dataVal=2,
+        };
+        toTest->instructionList[0] = inst;
+    }
+    { // TOP:
+        int* labelLoc = malloc(sizeof(int));
+        *labelLoc = 1;
+        Instruction inst = {
+            .operation=LABEL,
+            .src.type=STRING,
+            .src.value.dataStr=labelName,
+        };
+        hashmap_put(&toTest->labelMap, labelName, strlen(labelName), labelLoc);
+
+        toTest->instructionList[1] = inst;
+    }
+    { // JRO ACC
+        Instruction inst = {
+            .operation=JRO,
+
+            .src.type=PORT,
+            .src.value.dataPort=ACC,
+        };
+        toTest->instructionList[2] = inst;
+    }
+    { // ADD 1
+        Instruction inst = {
+            .operation=ADD,
+
+            .src.type=NUMBER,
+            .src.value.dataVal=1,
+        };
+        toTest->instructionList[3] = inst;
+    }
+    { // NEG
+        Instruction inst = {
+            .operation=NEG,
+        };
+        toTest->instructionList[4] = inst;
+    }
+    { // JRO ACC
+        Instruction inst = {
+            .operation=JRO,
+
+            .src.type=PORT,
+            .src.value.dataPort=ACC,
+        };
+        toTest->instructionList[5] = inst;
+    }
+    { // NEG
+        Instruction inst = {
+            .operation=NEG,
+        };
+        toTest->instructionList[6] = inst;
+    }
+    { // JMP T
+        Instruction inst = {
+            .operation=JMP,
+
+            .src.type=STRING,
+            .src.value.dataStr=labelName,
+        };
+        toTest->instructionList[7] = inst;
+    }
+
+    toTest->instructionCount = 8;
+
+    for (int _ = 0; _ < 25; _++) {
+        node_tick(toTest);
+    }
+
+    cr_expect_eq(toTest->instructionPointer, 2);
     cr_expect_eq(toTest->ACC, 1);
+
+}
+
+Test(NODE_FUNCTIONALITY, JRO_OUT_OF_BOUNDS_BEG, .init=setup_node, .fini=teardown_node) {
+    { // ADD 2
+        Instruction inst = {
+            .operation=ADD,
+
+            .src.type=NUMBER,
+            .src.value.dataVal=2,
+        };
+        toTest->instructionList[0] = inst;
+    }
+    { // JRO -5
+        Instruction inst = {
+            .operation=JRO,
+
+            .src.type=NUMBER,
+            .src.value.dataVal=-5,
+        };
+        toTest->instructionList[1] = inst;
+    }
+
+    toTest->instructionCount = 2;
+
+    for (int _ = 0; _ < 8; _++) {
+        node_tick(toTest);
+    }
+
+    cr_expect_eq(toTest->ACC, 8);
     cr_expect_eq(toTest->instructionPointer, 0);
 }
 
-// Test(NODE_JUMP, JNZ, .init=setup_node, .fini=teardown_node) {
+Test(NODE_FUNCTIONALITY, JRO_OUT_OF_BOUNDS_END, .init=setup_node, .fini=teardown_node) {
+    { // ADD 2
+        Instruction inst = {
+            .operation=ADD,
 
-// }
+            .src.type=NUMBER,
+            .src.value.dataVal=2,
+        };
+        toTest->instructionList[0] = inst;
+    }
+    { // JRO -1
+        Instruction inst = {
+            .operation=JRO,
 
+            .src.type=NUMBER,
+            .src.value.dataVal=5,
+        };
+        toTest->instructionList[1] = inst;
+    }
 
-// Test(NODE_JUMP, JGZ, .init=setup_node, .fini=teardown_node) {
-    
-// }
+    toTest->instructionCount = 2;
 
+    for (int _ = 0; _ < 5; _++) {
+        node_tick(toTest);
+    }
 
-// Test(NODE_JUMP, JLZ, .init=setup_node, .fini=teardown_node) {
-    
-// }
+    cr_expect_eq(toTest->ACC, 2);
+    cr_expect_eq(toTest->instructionPointer, 1);
+}
 
+Test(NODE_FUNCTIONALITY, JRO_JUMP_BACK_LABEL, .init=setup_node, .fini=teardown_node) {
+    char* labelName = strdup("TOP");
+    { // ADD 2
+        Instruction inst = {
+            .operation=ADD,
 
-// Test(NODE_JUMP, JRO, .init=setup_node, .fini=teardown_node) {
-    
-// }
+            .src.type=NUMBER,
+            .src.value.dataVal=2,
+        };
+        toTest->instructionList[0] = inst;
+    }
+    { // TOP:
+        int* labelLoc = malloc(sizeof(int));
+        *labelLoc = 1;
+        Instruction inst = {
+            .operation=LABEL,
+            .src.type=STRING,
+            .src.value.dataStr=labelName,
+        };
+        hashmap_put(&toTest->labelMap, labelName, strlen(labelName), labelLoc);
+
+        toTest->instructionList[1] = inst;
+    }
+    { // JRO -5
+        Instruction inst = {
+            .operation=JRO,
+
+            .src.type=NUMBER,
+            .src.value.dataVal=-1,
+        };
+        toTest->instructionList[2] = inst;
+    }
+
+    toTest->instructionCount = 3;
+
+    // If this is not added properly, the rest will not work
+    cr_assert_eq(*(int*)hashmap_get(&toTest->labelMap, labelName, strlen(labelName)), 1);
+
+    for (int _ = 0; _ < 10; _++) {
+        node_tick(toTest);
+    }
+    free(labelName);
+    cr_expect_eq(toTest->ACC, 8);
+    cr_expect_eq(toTest->instructionPointer, 1);
+}
+
+Test(NODE_FUNCTIONALITY, JRO_JUMP_FORWARD_LABEL, .init=setup_node, .fini=teardown_node) {
+    char* labelName = strdup("TOP");
+    { // JRO 1
+        Instruction inst = {
+            .operation=JRO,
+
+            .src.type=NUMBER,
+            .src.value.dataVal=1,
+        };
+        toTest->instructionList[0] = inst;
+    }
+    { // TOP:
+        int* labelLoc = malloc(sizeof(int));
+        *labelLoc = 1;
+        Instruction inst = {
+            .operation=LABEL,
+            .src.type=STRING,
+            .src.value.dataStr=labelName,
+        };
+        hashmap_put(&toTest->labelMap, labelName, strlen(labelName), labelLoc);
+
+        toTest->instructionList[1] = inst;
+    }
+    { // ADD 5
+        Instruction inst = {
+            .operation=ADD,
+
+            .src.type=NUMBER,
+            .src.value.dataVal=5,
+        };
+        toTest->instructionList[2] = inst;
+    }
+    { // NEG
+        Instruction inst = {
+            .operation=NEG,
+        };
+        toTest->instructionList[3] = inst;
+    }
+    { // JRO ACC
+        Instruction inst = {
+            .operation=JRO,
+
+            .src.type=PORT,
+            .src.value.dataPort=ACC,
+        };
+        toTest->instructionList[4] = inst;
+    }
+
+    toTest->instructionCount = 5;
+
+    // If this is not added properly, the rest will not work
+    cr_assert_eq(*(int*)hashmap_get(&toTest->labelMap, labelName, strlen(labelName)), 1);
+
+    for (int _ = 0; _ < 15; _++) {
+        node_tick(toTest);
+    }
+    free(labelName);
+    cr_expect_eq(toTest->ACC, 0);
+    cr_expect_eq(toTest->instructionPointer, 4);
+}
