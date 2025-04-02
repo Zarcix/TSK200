901f937661b42997673295a85d6606f992f849c3
Zarcix <mrslime43@gmail.com>
2025-04-02 00:37:29 -0700

    Added better sleep timing for TSK

--- src/tsk.c
+++ src/tsk.c
@@ -1,9 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <pthread.h>
 #include <argp.h>
-
+#include <time.h>
 #include "./tsk/node.h"
 #include "./tsk_misc/tsk_loader.h"
 
@@ -13,14 +12,20 @@ static char* COMMAND_PATH = "";
 
 static int NODE_MAX_OUTPUTS = 0;
 static int TICK_DELAY = 0;
+int milliseconds = 250;
 
 int main(int argc, char **argv) {
     Node *test = malloc(sizeof(Node));
     node_init(test);
     tsksrc_to_node(test, "test");
+
+    struct timespec ts;
+    ts.tv_sec = milliseconds / 1000;
+    ts.tv_nsec = (milliseconds % 1000) * 1000000;
+
     while (1) {
         node_tick(test);
         printf("Res: %d\n", test->ACC);
-        sleep(1);
+        nanosleep(&ts, NULL);
     }
 }
