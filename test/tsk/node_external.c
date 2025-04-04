b3c44dba04b191dd257b386597e08fbd4ad1e7bb
Zarcix <mrslime43@gmail.com>
2025-04-04 00:48:33 -0700

    Started external testing

---
+++ test/tsk/node_external.c
@@ -0,0 +1,70 @@
+#include <criterion/criterion.h>
+#include <criterion/assert.h>
+
+#include "../test_statics.h"
+
+#include "../../src/tsk/node.h"
+
+Test(NODE_FUNCTIONALITY, WRITE_ACC) {
+
+}
+
+Test(NODE_FUNCTIONALITY, WRITE_NIL) {
+    
+}
+
+Test(NODE_FUNCTIONALITY, WRITE_LAST) {
+    
+}
+
+Test(NODE_FUNCTIONALITY, WRITE_ANY) {
+    
+}
+
+Test(NODE_FUNCTIONALITY, WRITE_LEFT) {
+    
+}
+
+Test(NODE_FUNCTIONALITY, WRITE_RIGHT) {
+    
+}
+
+Test(NODE_FUNCTIONALITY, WRITE_UP) {
+    
+}
+
+Test(NODE_FUNCTIONALITY, WRITE_DOWN) {
+    
+}
+
+Test(NODE_FUNCTIONALITY, READ_ACC) {
+
+}
+
+Test(NODE_FUNCTIONALITY, READ_NIL) {
+    
+}
+
+Test(NODE_FUNCTIONALITY, READ_LAST) {
+    
+}
+
+Test(NODE_FUNCTIONALITY, READ_ANY) {
+    
+}
+
+Test(NODE_FUNCTIONALITY, READ_LEFT) {
+    
+}
+
+Test(NODE_FUNCTIONALITY, READ_RIGHT) {
+    
+}
+
+Test(NODE_FUNCTIONALITY, READ_UP) {
+    
+}
+
+Test(NODE_FUNCTIONALITY, READ_DOWN) {
+    
+}
