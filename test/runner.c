6688ab56ef37a9a00ca61a342bfd4ee3b8913bb2
Zarcix <mrslime43@gmail.com>
2025-05-01 17:41:57 -0700

    Add CMake

--- test/runner.c
+++ test/runner.c
@@ -6,7 +6,3 @@
 #include "./tsk/node_jump.c"
 #include "./tsk/node_internal.c"
 #include "./tsk/node_external.c"
-
-// TSK Loader Tests
-#include "./tsk_loader/loader_math.c"
-#include "./tsk_loader/loader_labels.c"
