639310b0650baa1ae8dd88fb6c339f7d475b6cfc
Zarcix <mrslime43@gmail.com>
2025-06-08 21:29:46 -0700

    Started initial OS devel

--- tsk/src/args.h
+++ tsk/src/args.h
@@ -1,7 +1,8 @@
 #ifndef PROG_ARGS
 #define PROG_ARGS
 
-int parse_opt(int key, char* arg, struct argp_state *state);
+int parse_opt(int key, char *arg, struct argp_state *state);
 int parse_args(int argc, char **argv);
 
 #endif
+
